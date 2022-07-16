dc65dd7564a511496ba95ffa9a1eadf746ee28ab
Bryan Zhang <40810357+Bryanjlz@users.noreply.github.com>
2022-07-16 00:28:04 -0400

    basic anchoring

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -3,19 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Drag : MonoBehaviour {
-    bool isDragging = false;
+	Vector2 currentAnchor;
+	bool isDragging = false;
 
+    private void Start() {
+		currentAnchor = transform.position;
+    }
     private void Update() {
-        if (isDragging) {
-            transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            if (Input.GetMouseButtonUp (0)) {
-                isDragging = false;
-            } 
+		if (isDragging) {
+			transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			if (Input.GetMouseButtonUp(0)) {
+				Release();
+			} 
+		}
+	}
+	private void OnMouseOver() {
+		if (Input.GetMouseButtonDown(0)) {
+			isDragging = true;
+		}
+	}
+
+	private void Release() {
+		isDragging = false;
+		foreach (GameObject anchor in GameObject.FindGameObjectsWithTag("Anchor")) {
+			if (Vector2.Distance(transform.position, anchor.transform.position) <= 1.02f) {
+				currentAnchor = anchor.transform.position;
+				transform.position = (Vector2) anchor.transform.position;
+				return;
+            }
         }
-    }
-    private void OnMouseOver() {
-        if (Input.GetMouseButtonDown(0)) {
-            isDragging = true;
-        }
-    }
+		transform.position = currentAnchor;
+	}
 }
