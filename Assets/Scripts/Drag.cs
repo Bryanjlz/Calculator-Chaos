bea09c9e8b78da5fa617d19f462e983ccdaa4a63
Bryan Zhang <40810357+Bryanjlz@users.noreply.github.com>
2022-07-16 00:30:44 -0400

    woop forgot to save

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -6,10 +6,13 @@ public class Drag : MonoBehaviour {
 	Vector2 currentAnchor;
 	bool isDragging = false;
 
-    private void Start() {
+	// Starting Anchor
+	private void Start() {
 		currentAnchor = transform.position;
-    }
-    private void Update() {
+	}
+
+	private void Update() {
+		// Drag
 		if (isDragging) {
 			transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			if (Input.GetMouseButtonUp(0)) {
@@ -17,6 +20,8 @@ public class Drag : MonoBehaviour {
 			} 
 		}
 	}
+	
+	// Drag
 	private void OnMouseOver() {
 		if (Input.GetMouseButtonDown(0)) {
 			isDragging = true;
@@ -25,13 +30,16 @@ public class Drag : MonoBehaviour {
 
 	private void Release() {
 		isDragging = false;
+		// Look for close anchor to go to
 		foreach (GameObject anchor in GameObject.FindGameObjectsWithTag("Anchor")) {
 			if (Vector2.Distance(transform.position, anchor.transform.position) <= 1.02f) {
 				currentAnchor = anchor.transform.position;
 				transform.position = (Vector2) anchor.transform.position;
 				return;
-            }
-        }
+			}
+		}
+
+		// If none, go back to prev
 		transform.position = currentAnchor;
 	}
 }
