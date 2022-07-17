b51b40f37ad4c298feb0be4b546f4c87a4b77f8e
Bryan Zhang <40810357+Bryanjlz@users.noreply.github.com>
2022-07-17 00:17:01 -0400

    Added Level Data

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class LevelData : ScriptableObject {
+	public int numRounds;
+
+	[Space(15)]
+	public int genCount;
+	public List<int> generationPool;
+
+	[Space(15)]
+	public List<int> targets;
+
+	[Space(15)]
+	public bool isRandomEquation;
+	[Tooltip("Only used if random")]
+	public int numEquations;
+
+	[Space(10)]
+	[Tooltip("Only used if not random")]
+	public List<Operator> operators;
+
+	[Space(10)]
+	public List<string> text;
+	public List<Vector2> pos;
+}
